6bcdf58a3b8b7be1780fb8ac25c1da3ed3cd60a6
WedyDQ10 <hosigumayuugi@gmail.com>
2014-04-25 09:32:36 +0900

    Support 4.4

--- src/com/wedy/auto20mp/NotificationiconPatcher.java
+++ src/com/wedy/auto20mp/NotificationiconPatcher.java
@@ -3,6 +3,8 @@ package com.wedy.auto20mp;
 import android.content.res.XModuleResources;
 import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookZygoteInit;
+import android.os.Build;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 
 public class NotificationiconPatcher implements IXposedHookZygoteInit, IXposedHookInitPackageResources{
@@ -22,7 +24,12 @@ public class NotificationiconPatcher implements IXposedHookZygoteInit, IXposedHo
 		}
 
 		XModuleResources modRes = XModuleResources.createInstance(modulePath, resparam.res);
+		if(Build.VERSION.SDK_INT <= 18){
 		resparam.res.setReplacement(PACKAGE_ESF, "bool", "use_max_still_capture_resolution_as_default_regardless_of_display_aspect", true);
+	XposedBridge.log("20MP: detected JB");
+		}
+		resparam.res.setReplacement(PACKAGE_ESF, "array", "ux_recommended_resolution_array_main_superiorauto_20m", modRes.fwd(R.array.ux_recommended_resolution_array_main_superiorauto_20m));
+		XposedBridge.log("20MP: detected KK");
 	}
 
 }
